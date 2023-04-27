bcb4dbe6d41efa15f4f802edbe724a9dd935894a
Halysson Freitas <halyssonfreitas@gmail.com>
2023-04-27 12:32:48 -0300

    :art: list with map

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 
 function List(){
+  const tarefas = [{
+    tarefa: 'React',
+    tempo: '02:00:00'
+  }, {
+    tarefa: 'Java Script',
+    tempo: '01:00:00'
+  }, {
+    tarefa: 'Type Script',
+    tempo: '03:00:00'
+  }]
   return (
     <aside>
       <h2>Estudos do dia</h2>
       <ul>
-        <li>
-          <h3>
-            React
-          </h3>
-          <span>
-            02:00:00
-          </span>
-        </li>
-        <li>
-          <h3>
-            Java Script
-          </h3>
-          <span>
-            01:00:00
-          </span>
-        </li>
+        {tarefas.map((item, index) => (
+          <li key={index}>
+            <h3>
+              {item.tarefa}
+            </h3>
+            <span>
+              {item.tempo}
+            </span>
+          </li>
+        ))}
       </ul>
     </aside>
   )
