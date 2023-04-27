f252cb889e10d9724195dc41eb0b2a2670d9d298
Halysson Freitas <halyssonfreitas@gmail.com>
2023-04-27 19:40:46 -0300

    :sparkles: control the form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,9 +3,19 @@ import Button from "../Button";
 import style from './Form.module.scss'
 
 class Form extends React.Component {
+  state = {
+    tarefa: "",
+    time: "00:00"
+  }
+
+  adicionarTarefa(event: React.FormEvent<HTMLFormElement>): void {
+    event.preventDefault()
+    console.log("state : ", this.state)
+  }
+
   render(): React.ReactNode {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">
             Adicione um novo estudo
@@ -15,6 +25,8 @@ class Form extends React.Component {
             name="tarefa"
             id="tarefa" 
             placeholder="O que você quer estudar ?"
+            value={this.state.tarefa}
+            onChange={event => this.setState({...this.state, tarefa: event.target.value})}
           />
         </div>
         <div className={style["inputContainer"]}>
@@ -25,6 +37,8 @@ class Form extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.time}
+            onChange={event => this.setState({...this.state, time: event.target.value})}
             id="tempo"
             min="00:00:00"
             max="01:30:00"
