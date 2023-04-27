4b5f4f4264c1a208bd37854fb23da6c878132ef7
Halysson Freitas <halyssonfreitas@gmail.com>
2023-04-27 12:48:28 -0300

    :lipstick: css inline

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 class Button extends React.Component {
   render(): React.ReactNode {
+    const backgroundColor = 'green'
+    const style = {
+      backgroundColor
+    }
     return (
-      <button>
+      <button style={style}>
         Button
       </button>
     )
