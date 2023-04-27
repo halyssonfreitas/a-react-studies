12f4623c7a41834cc591218c62b6785e2e37cf1d
Halysson Freitas <halyssonfreitas@gmail.com>
2023-04-27 16:24:52 -0300

    ✨ add the first props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import style from './Button.module.scss'
 
-class Button extends React.Component {
+class Button extends React.Component<{ children: string}> {
   render(): React.ReactNode {
     return (
       <button className={style.botao}>
-        Button
+        {this.props.children}
       </button>
     )
   }
