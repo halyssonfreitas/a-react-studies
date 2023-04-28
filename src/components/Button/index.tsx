162f6da7ba13ea3659bb1c6c2b505f96bc359bad
Halysson Freitas <halyssonfreitas@gmail.com>
2023-04-28 08:01:54 -0300

    :sparkles: add button type

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import style from './Button.module.scss'
 
-class Button extends React.Component<{ children: string}> {
+class Button extends React.Component<{
+  children: string,
+  type?: "button" | "submit" | "reset" | undefined
+  }> {
   render(): React.ReactNode {
+    const { type = "button" } = this.props
     return (
-      <button className={style.botao}>
+      <button type={type} className={style.botao}>
         {this.props.children}
       </button>
     )
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -45,7 +45,7 @@ class Form extends React.Component {
             required
           />
         </div>
-        <Button>
+        <Button type="submit">
           Adicionar
         </Button>
       </form>
